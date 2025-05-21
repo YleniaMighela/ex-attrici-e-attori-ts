488d080dd7076ddf541407988050132c6ca0b31c
Ylenia <yleniamighela@gmail.com>
2025-05-21 15:12:13 +0200

    milestone 01

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
-console.log("ciao");
+// MILESTONE 01
+// Crea un type alias Person per rappresentare una persona generica
+type Person = {
 
+  readonly id: number,
+  readonly name: string,
+  birth_year: number,
+  death_year?: number,
+  biography: string,
+  image: string,
+
+}
